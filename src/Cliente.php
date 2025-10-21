66c9b31c998a7ac720012725eaccbce931b01269
isaxo-dev <isaxo123321@gmail.com>
2025-10-21 17:23:02 -0300

    metodo construtor iniciado

--- src/Cliente.php
+++ src/Cliente.php
@@ -1,18 +1,13 @@
 <?php
 class Cliente {
-    /* Atributos da classe: representam características que os objetos deverão ter */
     public string $nome;
     public int $idade;
     public string $email;
 
-    /* Método responsável por exibir os dados de um objeto */
-
-    // Usamos void para sinalizar que o método não tem return
-    public function mostrarDados():void {
-        echo "  <div>
-                    <h4> $this->nome </h4>
-                    <p><b>E-mail de contato:</b> $this->email </p>
-                    <p><b>Idade: </b> $this->idade anos</p>
-                </div>";
+    /* Método construtor: responsável por 'construir' um objeto COM seus dados no momento da criação */
+    public function __construct( string $ValorDoNome, int $ValorDaIdade, string $ValorDoEmail ) {
+        
     }
+    
+
 }

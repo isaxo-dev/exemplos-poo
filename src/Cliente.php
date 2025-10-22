08deca2c840048b4ee37102ee505d3df06820400
isaxo-dev <isaxo123321@gmail.com>
2025-10-22 14:25:02 -0300

    Parâmetro "telefone" adicionado

--- src/Cliente.php
+++ src/Cliente.php
@@ -4,13 +4,17 @@ class Cliente {
     public int $idade;
     public string $email;
 
+    // Telefone é opcional, ou seja, caso não seja informado ficará valendo null
+    public ?string $telefone; // ? Indica que este atributo PODE ser NULL
+
     /* Método construtor: responsável por 'construir' um objeto COM seus dados no momento da criação */
-    public function __construct( string $ValorDoNome, int $ValorDaIdade, string $ValorDoEmail ) {
+    public function __construct( string $valorDoNome, int $valorDaIdade, string $valorDoEmail, string $valorDoTelefone ) {
         
         /* Atribuindo os valores recebidos através dos parâmetros do construtor, aos atributos da classe */
-        $this->nome = $ValorDoNome;
-        $this->idade = $ValorDaIdade;
-        $this->email = $ValorDoEmail;
+        $this->nome = $valorDoNome;
+        $this->idade = $valorDaIdade;
+        $this->email = $valorDoEmail;
+        $this->telefone = $valorDoTelefone;
     }
     
 
